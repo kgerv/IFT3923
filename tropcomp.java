7696ef159f74e15da16141399f48f000da8d1813
GabHazan <gabriel.hazan@umontreal.ca>
2023-10-04 22:55:43 -0400

    tropcomp update
    should be good now, requires testing

--- tropcomp.java
+++ tropcomp.java
@@ -11,31 +11,25 @@ public class tropcomp{
         Tls tls = new Tls(project_path);
         data = tls.explorelevel();
 
+        int numToKeep = data.size() * (seuil / 100.0);
+
         // Sort the array of lists based on values at index 3 (integer value)
         data.sort(Comparator.comparing(item -> Integer.parseInt(item[3]), Comparator.reverseOrder()));
 
-        // Filter the sorted list to include only values above seuil for tloc
-        List<String[]> filteredListTloc = new ArrayList<>();
-        for (String[] item : data) {
-            int tloc = Integer.parseInt(item[3]);
-            if (tloc >= (seuil / 100.0 * tloc)) {
-                filteredListTloc.add(item);
-            }
-        }
+        // Filter the sorted list to include only values above threshold
+        List<String[]> filteredListTloc = data.subList(0, numToKeep);
 
-        // Sort the filtered list based on values at index 5 (double value)
-        filteredListTloc.sort(Comparator.comparing(item -> Double.parseDouble(item[5]), Comparator.reverseOrder()));
+        // Sort the array of lists based on values at index 5 (double value)
+        data.sort(Comparator.comparing(item -> Double.parseDouble(item[5]), Comparator.reverseOrder()));
 
         // Filter the sorted list to include only values above seuil for tcmp
-        List<String[]> filteredListTcmp = new ArrayList<>();
-        for (String[] item : filteredListIndex3) {
-            double tcmp = Double.parseDouble(item[5]);
-            if (tcmp >= (seuil / 100.0 * tcmp)) {
-                filteredListTcmp.add(item);
-            }
-        }
+        List<String[]> filteredListTcmp = data.subList(0, numToKeep);
+
+        // Retain only the items inn common
+        filteredListTloc.retainAll(filteredListTcmp);
+
         // Print the final filtered list
-        for (String[] item : filteredListIndex5) {
+        for (String[] item : filteredListTloc) {
             System.out.println(Arrays.toString(item));
         }
     }
