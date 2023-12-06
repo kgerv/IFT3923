42af6bba0fba1e53b6bfaff67212579548ab4dbf
GabHazan <gabriel.hazan@umontreal.ca>
2023-12-06 15:26:14 -0500

    qq tests pour mainwindow

--- TP4/currency-converter-in-java/src/test/MainWindowTest.java
+++ TP4/currency-converter-in-java/src/test/MainWindowTest.java
@@ -1,11 +1,23 @@
 package test;
 
+import currencyConverter.Currency;
+import currencyConverter.MainWindow;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MainWindowTest {
 
     @Test
     public void testConvert() {
+        ArrayList<Currency> currencies = Currency.init();
+
+        assertEquals(-2066666.46, MainWindow.convert("US Dollar", "Euro", currencies, -2222222.0));
+        assertEquals(-0.08, MainWindow.convert("US Dollar", "Euro", currencies, -0.09));
+        assertEquals(0.0, MainWindow.convert("US Dollar", "Euro", currencies, 0.0));
+        assertEquals(465000.0, MainWindow.convert("US Dollar", "Euro", currencies, 500000.0));
+        assertEquals(930000.0, MainWindow.convert("US Dollar", "Euro", currencies, 1000000.0));
+        assertEquals(930000.009, MainWindow.convert("US Dollar", "Euro", currencies, 1000000.01));
+        assertEquals(20666666.46, MainWindow.convert("US Dollar", "Euro", currencies, 2222222.0));
 
     }
 }
