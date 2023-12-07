eaa7c2f7f776d11e1cea964eae37f870cc9b729e
Killian <113630849+kgerv@users.noreply.github.com>
2023-12-06 22:02:21 -0500

    Update rapport.tex & MainWindowTest.java
    - rapport.tex: made expected behavior of an invalid input clearer
    - MainWindow.java: changed test of value that should not convert from assertEquals to assertNotEquals

--- TP4/currency-converter-in-java/src/test/MainWindowTest.java
+++ TP4/currency-converter-in-java/src/test/MainWindowTest.java
@@ -8,9 +8,14 @@ import java.util.ArrayList;
 
 public class MainWindowTest {
 
+    /**
+     * Following the given specification amounts in [0, 1 000 000] are valid and should be converted and any other
+     * amounts should not. Only currencies in {USD, CAD, GBP, EUR, CHF, AUD} are accepted.
+     */
     @Test
     public void testConvert() {
         ArrayList<Currency> currencies = new ArrayList<>();
+        // assumes that constructor for Currency will not change
         Currency usd = new Currency("US Dollar", "USD");
         usd.setExchangeValues("EUR", 0.93);
 
@@ -20,13 +25,20 @@ public class MainWindowTest {
         currencies.add(usd);
         currencies.add(eur);
 
-        assertEquals(-2066666.46, MainWindow.convert("US Dollar", "Euro", currencies, -2222222.0));
-        assertEquals(-0.08, MainWindow.convert("US Dollar", "Euro", currencies, -0.09));
+        // testing behavior with unaccepted currency, should not convert
+        assertEquals(-1.0 , MainWindow.convert("Japanese Yen", "US Dollar", currencies, 100.0));
+
+        // testing behavior according to amounts
+        // typical and border values in [0, 1 000 000], should convert
         assertEquals(0.0, MainWindow.convert("US Dollar", "Euro", currencies, 0.0));
         assertEquals(465000.0, MainWindow.convert("US Dollar", "Euro", currencies, 500000.0));
         assertEquals(930000.0, MainWindow.convert("US Dollar", "Euro", currencies, 1000000.0));
-        assertEquals(930000.009, MainWindow.convert("US Dollar", "Euro", currencies, 1000000.01));
-        assertEquals(20666666.46, MainWindow.convert("US Dollar", "Euro", currencies, 2222222.0));
+
+        // typical and border values not in [0, 1 000 000], should not convert
+        assertNotEquals(-2066666.46, MainWindow.convert("US Dollar", "Euro", currencies, -2222222.0));
+        assertNotEquals(-0.08, MainWindow.convert("US Dollar", "Euro", currencies, -0.09));
+        assertNotEquals(930000.01, MainWindow.convert("US Dollar", "Euro", currencies, 1000000.01));
+        assertNotEquals(20666666.46, MainWindow.convert("US Dollar", "Euro", currencies, 2222222.0));
 
     }
 }
